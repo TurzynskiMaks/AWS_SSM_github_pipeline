b56e85fcc52b66748ba7ec33874276a03f709c86
TurzynskiMaks <maksturzynski2137@gmail.com>
2025-11-01 15:14:02 +0100

    Adding another service to see if system is consistent

--- test2/app.py
+++ test2/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return '5000', 200
+    return '5001', 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5001)
